f136fdd7e8f52c2cb45ee78ec17df2efd2117ffe
W1zard70r <114387864+W1zard70r@users.noreply.github.com>
2022-10-01 09:01:40 +0700

    Update python_pascal_trans.py

--- python_pascal_trans.py
+++ python_pascal_trans.py
@@ -1,2 +1,4 @@
 print("вывод ткста на экран") #аналог команды Writeln
 input("введите число - ") #аналог команды Readln
+a=int() #аналог a:=число var intger;
+while i in a,b,c,d: #версия while для питонa
